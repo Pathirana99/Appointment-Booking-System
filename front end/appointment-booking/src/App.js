f30f11afff947b4dcd63f068a4955b7290167b74
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-02-07 19:47:50 +0530

    add login design

--- front end/appointment-booking/src/App.js
+++ front end/appointment-booking/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import React from 'react';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
+import User from './Pages/User';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Admin from './Pages/Admin';
 
 function App() {
   return (
-    <div className="App">
-     <>hi there</>
-    </div>
+    <Router>
+     
+      <div>
+        <Routes>
+          <Route path='/' element={<Login/>} />
+          <Route path='/User' element={<User/>} />
+          <Route path='/SignUp' element={<SignUp/>} />
+          <Route path='/Admin' element={<Admin/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ front end/appointment-booking/src/Pages/Login.js
@@ -0,0 +1,41 @@
+import "./login.css";
+
+export default function Login() {
+
+  return (
+    <div className="login">
+    <img
+            src="/images/nature.jpg"
+            alt=""
+          />
+      <div className="loginContent">
+        <div className="loginHeader">
+          <h2>Login</h2>
+        </div>
+        <div className="loginForm">
+          <div className="formSection">
+            <label>Email</label>
+            <input
+              type="text"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="formSection">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button className="loginButton">
+            Login
+          </button>
+          <h9>If you don't have an account, please sign up</h9>
+          <button className="signUpButton">
+            SignUp
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
