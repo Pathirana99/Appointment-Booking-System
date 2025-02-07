cfd37362448e6acf88ffd671bff9a2344afc3ec9
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-02-07 21:36:23 +0530

    add user page

--- front end/appointment-booking/src/Pages/User.js
+++ front end/appointment-booking/src/Pages/User.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import "./user.css";
 
 export default function User() {
+
   return (
-    <div>
-      <>User</>
+    <div className="home">
+      <div className="mainContent">
+      <div className="profilePic">
+          <div className="profileLetter">S</div>
+        </div>
+        <div className="header">
+          <div></div>
+        </div>
+        <button className="addButton" >
+            ADD
+          </button>
+        <div className="form">
+          <div className="section">
+            <input
+              type="text"
+              />
+          </div>
+          <div className="section">
+            <input
+              type="text"
+              />
+          </div>
+          <div className="section">
+            <input
+              type="text"
+              />
+          </div>
+          <div className="section">
+            <input
+              type="text"
+              />
+          </div>
+          <div className="section">
+            <input
+              type="text"
+              />
+          </div>
+          <button className="logOut">
+          Log Out
+        </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
