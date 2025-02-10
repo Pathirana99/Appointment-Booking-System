20e12133e720de2272835006d2d9e190a37e16c5
Sunith-P-Pathirana <sunithkaushalya.pp@gmail.com>
2025-02-10 11:10:23 +0530

    edit

--- front end/appointment-booking/src/Pages/Login.js
+++ front end/appointment-booking/src/Pages/Login.js
@@ -1,7 +1,85 @@
 import "./login.css";
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
 
 export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  const validateEmail = (email) => {
+    return emailRegex.test(email);
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    setError("");
+
+    if (!email && !password) {
+      setError("Email and password cannot be empty.");
+      return;
+    } else if (email && !password) {
+      setError("password cannot be empty.");
+      return;
+    } else if (!email && password) {
+      setError("email cannot be empty.");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/loginUser/login",
+        {
+          email: email,
+          password: password,
+        }
+      );
+
+      const { jwt, role } = response.data;
+
+      localStorage.setItem("jwt", jwt);
+      localStorage.setItem("role", role);
+      localStorage.setItem("password", password);
+
+      const decodedJwt = jwtDecode(jwt);
+      const { username, email: userEmail } = decodedJwt;
+
+      localStorage.setItem("username", username);
+      localStorage.setItem("userEmail", userEmail);
+
+      if (role === "USER") {
+        navigate("/User");
+      } else if (role === "ADMIN") {
+        navigate("/Admin");
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      setError("Invalid email or password.");
+    }
+  };
+
+  const handleSignUp = () => {
+    navigate("/SignUp");
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleLogin(event);
+    }
+  };
+
 
   return (
     <div className="login">
@@ -15,10 +93,14 @@ export default function Login() {
         </div>
         <div className="loginForm">
           <div className="formSection">
+           {error && <p className="errorMessage">{error}</p>}
             <label>Email</label>
             <input
               type="text"
               placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onKeyDown={handleKeyDown}
             />
           </div>
           <div className="formSection">
@@ -26,13 +108,16 @@ export default function Login() {
             <input
               type="password"
               placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onKeyDown={handleKeyDown}
             />
           </div>
-          <button className="loginButton">
+          <button className="loginButton" onClick={handleLogin}>
             Login
           </button>
           <h9>If you don't have an account, please sign up</h9>
-          <button className="signUpButton">
+          <button className="signUpButton" onClick={handleSignUp}>
             SignUp
           </button>
         </div>
--- front end/appointment-booking/src/Pages/User.js
+++ front end/appointment-booking/src/Pages/User.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./user.css";
+import { jwtDecode } from "jwt-decode";
+//import axios from "axios";
 
 export default function User() {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  //const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    const jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      const decodedJwt = jwtDecode(jwt);
+      setUsername(decodedJwt.username);
+      setEmail(decodedJwt.email);
+      //setUserId(decodedJwt.userId);
+    }
+  }, []);
+  const handleLogout = () => {
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("username");
+    localStorage.removeItem("userEmail");
+
+    window.location.href = "/";
+  };
+
   const appoinments = [
     { id: 1, name: "sunith", contact: "0776117695", date: "2024.02.02", time:"08.00" },
     ];
@@ -10,11 +34,11 @@ export default function User() {
     <div className="home">
       <div className="mainContent">
       <div className="profilePic">
-          <div className="profileLetter">S</div>
+          <div className="profileLetter">{username.charAt(0)}</div>
         </div>
-        <div className="header">
-          <div></div>
-        </div>
+        
+        <div className="userName">{email}</div>
+      
         <button className="addButton" >
         To get an appointment
           </button>
@@ -43,7 +67,7 @@ export default function User() {
             ))}
           </tbody>
         </table>
-          <button className="logOut">
+          <button className="logOut" onClick={handleLogout}>
           Log Out
         </button>
         </div>
